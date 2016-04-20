4776251c58044778d81107749d59719eef5f7b96
ruslan <qwe@gmail.com>
2016-04-20 23:17:45 +0200

    Some refactor in hunter module

--- SimpleIrcBot/Modules/HunterModule.cs
+++ SimpleIrcBot/Modules/HunterModule.cs
@@ -5,27 +5,28 @@ namespace SimpleIrcBot.Modules
 {
    public class HunterModule
    {
-       private string huntedNickPersone;
-       private string pathToFile;
+        private string huntedNickPersone;
+        private string pathToFile;
+        private OutputConsoleModule consoleNotifer;
 
-       public HunterModule(string nickname, string path)
-       {
-           huntedNickPersone = nickname;
-           //pathToFile = @"C:\Keys\ParagonKeys.txt";
-       }
+        public HunterModule(string nickname, string path, OutputConsoleModule consoleModule)
+        {
+            huntedNickPersone = nickname;
+            consoleNotifer = consoleModule;
+            //pathToFile = @"C:\Keys\ParagonKeys.txt";
+        }
 
-       public  void CatchKey(string nickname, string inputMessage)
-       {
-           if (nickname == huntedNickPersone && inputMessage.Contains("Early Access preview"))
-           {
-               using (var file = new StreamWriter(@"C:\Keys\ParagonKeys.txt", true))
-               {
-                   file.WriteLine(inputMessage);
-               }
-               Console.ForegroundColor = ConsoleColor.Red;
-               Console.WriteLine("Founded");
-               Console.ResetColor();
-           }
-       }
+        public void CatchKey(string nickname, string inputMessage)
+        {
+            if (nickname == huntedNickPersone && inputMessage.Contains("Early Access preview"))
+            {
+                using (var file = new StreamWriter(@"C:\Keys\ParagonKeys.txt", true))
+                {
+                    file.WriteLine(inputMessage);
+                }
+                consoleNotifer.HiglightMessage(ConsoleColor.Red, "Founded");
+                consoleNotifer.PlayImpMarch();
+            }
+        }
     }
 }
--- SimpleIrcBot/Program.cs
+++ SimpleIrcBot/Program.cs
@@ -30,16 +30,16 @@ namespace SimpleIrcBot
             };
 
             var twitchinfo = new TwitchInfo("irc.twitch.tv", 6667);
-            var bot = new Bot(twitchinfo, "your_bot_login", "your_bot_oauth:pass");
-            var botModule= new BotModule(bot);
+            var bot = new Bot(twitchinfo, "your_bot_login", "your_bot_oauth");
+            var botModule = new BotModule(bot);
             var consoleModule = new OutputConsoleModule();
             var respectPremUsers = new RespectPremiumUsersModule();
             var respectFriendUsers = new RespectFriendUsersModule();
             var parserModule = new ParserModule();
-            var hunterModule = new HunterModule("OfficialParagonBot","");
+            var hunterModule = new HunterModule("OfficialParagonBot", "",consoleModule);
 
-            var commandModule = new CommandModule(botModule,consoleModule,respectPremUsers,respectFriendUsers);
-            var monitormodule = new MonitoringModule(botModule,commandModule,parserModule);
+            var commandModule = new CommandModule(botModule, consoleModule, respectPremUsers, respectFriendUsers);
+            var monitormodule = new MonitoringModule(botModule, commandModule, parserModule);
 
             consoleModule.HiglightMessage(ConsoleColor.Green, "!!!!!!!!!!Стартую наблюдение!!!!!!!!!!!!");
             //monitormodule.StartListenChannel("your_channel");
